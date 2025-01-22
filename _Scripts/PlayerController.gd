fc670fde920ec305b1d4add659bc97e61c26ac7d
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2025-01-22 11:28:43 -0500

    Created the "Submarine" art and a basic, incomplete character controller. I clearly have more learning to do, and am prioritizing education over completion

---
+++ _Scripts/PlayerController.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 300
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	var velocity
+	if direction.length > 0:
+		direction = direction.normalized()
+		
+	velocity = direction * speed
